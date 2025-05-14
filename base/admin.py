e54c71c000ce1cd3a1749b9541d6e41b358012a1
jacoubakiz <J.akizi@icsl.me>
2025-05-14 12:43:23 +0300

    update

--- base/admin.py
+++ base/admin.py
@@ -36,10 +36,57 @@ admin.site.register(CustomUser, AdminCustomUser)
 admin.site.register(CodeVerification)
 
 
-admin.site.register(MedicalTest)
-admin.site.register(Bouquet)
 
-admin.site.register(Cart)
-admin.site.register(Order)
-admin.site.register(Analysis)
-admin.site.register(ResultsAnalysis)
+class AdminMedicalTest(admin.ModelAdmin):
+    list_display = ['test_name']
+    search_fields = ['test_name']
+    list_per_page = 25
+
+admin.site.register(MedicalTest, AdminMedicalTest)
+
+class AdminBouquet(admin.ModelAdmin):
+    list_display = ['bouquet_name', 'price', 'descreption']
+    search_fields = ['bouquet_name']
+    list_per_page = 25
+
+admin.site.register(Bouquet, AdminBouquet)
+
+class AdminCart(admin.ModelAdmin):
+    list_display = ['user']
+    list_per_page = 25
+
+admin.site.register(Cart, AdminCart)
+
+class AdminOrder(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'gender', 'phonenumber', 'total_price']
+    search_fields = ['phonenumber', 'full_name']
+    list_per_page = 25
+
+admin.site.register(Order, AdminOrder)
+
+class AdminAnalysis(admin.ModelAdmin):
+    list_display = ['user', 'medical_test', 'result', 'natural_value', 'evaluation', 'date']
+    search_fields = ['medical_test']
+    list_per_page = 25
+
+    fieldsets = (
+        ('Choice Result Analysis', 
+            {'fields':('resultsanalysis',)}
+        ),
+        ('Please Add detailes Analysis',
+            {'fields':('medical_test', 'result', 'natural_value', 'evaluation')}
+        ),
+    )
+
+    def user(self, obj):
+        return obj.resultsanalysis.user.username
+    
+admin.site.register(Analysis, AdminAnalysis)
+
+class AdminResultsAnalysis(admin.ModelAdmin):
+    list_display = ['user']
+    search_fields = ['search_keyword']
+    list_per_page = 25
+    def search_keyword(self, obj):
+        return obj.user.username
+admin.site.register(ResultsAnalysis, AdminResultsAnalysis)
